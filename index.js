3521f00fd91af3ddc25aa321a554dad7b8f34f24
Erik Johansson <erik.johansson87@gmail.com>
2016-07-14 08:25:04 +0200

    If empty string in .env just go on

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var envFile = fs.readFileSync(dir + path.sep + '.env', 'utf8');
 var envArray = envFile.split('\n');
 
 envArray.forEach(function(envKeyVal) {
+    if(envKeyVal.length == 0) return;
     var key = envKeyVal.split('=')[0].trim(),
         val = envKeyVal.split('=')[1].trim();
-
     process.env[key] = val;
 }, this);
