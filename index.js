66be32d3216bb0fa5fc257a7469df752e8c54720
Erik Johansson <erik.johansson87@gmail.com>
2016-08-08 15:32:18 +0200

    Check if file exist before reading it

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var fs = require('fs'),
     path = require('path'),
-    dir = path.dirname(require.main.filename);
+    dir = path.dirname(require.main.filename),
+    envfilepath = dir + path.sep + '.env';
 
-var envFile = fs.readFileSync(dir + path.sep + '.env', 'utf8');
-var envArray = envFile.split('\n');
+fs.access(envfilepath, fs.F_OK, function(err) {
+    if(err) return;
+    var envFile = fs.readFileSync(envfilepath, 'utf8');
+    var envArray = envFile.split('\n');
 
-envArray.forEach(function(envKeyVal) {
-    if(envKeyVal.length == 0 || envKeyVal.indexOf('=') == -1 || envKeyVal.indexOf('#') > -1) return;
-    var key = envKeyVal.split('=')[0].trim(),
-        val = envKeyVal.split('=')[1].trim();
-    process.env[key] = val;
-}, this);
+    envArray.forEach(function(envKeyVal) {
+        if(envKeyVal.length == 0 || envKeyVal.indexOf('=') == -1 || envKeyVal.indexOf('#') > -1) return;
+        var key = envKeyVal.split('=')[0].trim(),
+            val = envKeyVal.split('=')[1].trim();
+        process.env[key] = val;
+    }, this);
+});
