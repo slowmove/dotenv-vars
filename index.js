f7b0c3d00e53fa12193bf99367f9a6673e2391d0
Erik Johansson <ejohansson@c1mqt0p4g944.tv4.se>
2016-09-23 13:27:10 +0200

    don't crash if no .env exists

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ var fs = require('fs'),
     dir = path.dirname(require.main.filename),
     envfilepath = dir + path.sep + '.env';
 
-var envFile = fs.readFileSync(envfilepath, 'utf8');
-var envArray = envFile.split('\n');
+try {
+    var envFile = fs.readFileSync(envfilepath, 'utf8');
+    var envArray = envFile.split('\n');
 
-envArray.forEach(function(envKeyVal) {
-    if(envKeyVal.length == 0 || envKeyVal.indexOf('=') == -1 || envKeyVal.indexOf('#') > -1) return;
-    var key = envKeyVal.split('=')[0].trim(),
-        val = envKeyVal.split('=')[1].trim();
-    process.env[key] = val;        
-}, this);
+    envArray.forEach(function(envKeyVal) {
+        if(envKeyVal.length == 0 || envKeyVal.indexOf('=') == -1 || envKeyVal.indexOf('#') > -1) return;
+        var key = envKeyVal.split('=')[0].trim(),
+            val = envKeyVal.split('=')[1].trim();
+        process.env[key] = val;        
+    }, this);    
+} catch (error) {
+    
+}
